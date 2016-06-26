7e8a13738cad2404742d1a7a7acebb33f47503e8
miguelveces <mveces8@gmail.com>
2016-06-26 18:51:40 -0500

    commit inicial de la nueva aplicacion my little magic ap

---
+++ www/js/mymagicbook.js
@@ -0,0 +1,86 @@
+var x;
+x=$(document);
+x.ready(inicializarEventos);
+function inicializarEventos()
+{
+	var tema1, tema2, tema3, tema4, tema5, tema6, tema7, tema8, tema9, tema10, tema11, tema12, tema13, tema4, tema15;
+
+   tema1=$("#tema1");
+   tema2=$("#tema2");
+   tema3=$("#tema3");
+   tema4=$("#tema4");
+   tema5=$("#tema5");
+   tema6=$("#tema6");
+   tema7=$("#tema7");
+   tema8=$("#tema8");
+   tema9=$("#tema9");
+   tema10=$("#tema10");
+   tema11=$("#tema11");
+   tema12=$("#tema12");
+   tema13=$("#tema13");
+   tema14=$("#tema14");
+   tema15=$("#tema15");
+/*se llama al evento de los enlaces*/
+  tema1.click(tema_1);
+  tema2.click(tema_2);
+  tema3.click(tema_3);
+  tema4.click(tema_4);
+  tema5.click(tema_5);
+  tema6.click(tema_6);
+  tema7.click(tema_7);
+  tema8.click(tema_8);
+  tema9.click(tema_9);
+  tema10.click(tema_10);
+  tema11.click(tema_11);
+  tema12.click(tema_12);
+  tema13.click(tema_13);
+  tema14.click(tema_14);
+  tema15.click(tema_15);
+
+}
+
+function tema_1(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 1"
+}
+function tema_2(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 2"
+}
+function tema_3(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 3"
+}
+function tema_4(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 4"
+}
+function tema_5(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 5"
+}
+function tema_6(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 6"
+}
+function tema_7(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 7"
+}
+function tema_8(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 8"
+}
+function tema_9(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 9"
+}
+function tema_10(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 10"
+}
+function tema_11(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 11"
+}
+function tema_12(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 12"
+}
+function tema_13(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 13"
+}
+function tema_14(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 14"
+}
+function tema_15(){
+document.getElementById("contenido").innerHTML="En este tema entontraras fraces practicas referente a tema 15"
+}
